45251ff3244e3c8fee3bdf9f5834399a6a4b7d49
malakkmahfouz <147241014+malakkmahfouz@users.noreply.github.com>
2025-01-16 20:37:56 +0200

    Edit interface used for proxy

--- Model/IItemModel.php
+++ Model/IItemModel.php
@@ -3,7 +3,7 @@ interface IItemModel {
     public function add();
     public function edit();
     public function read();
-    public function remove($id);
-    public function view_all();
+    public static function remove($id);
+    public static function view_all();
 }
 ?>
