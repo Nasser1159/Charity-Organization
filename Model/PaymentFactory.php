7d90d732444e842c9925ab7a0a789b59e1754558
Michael-Adeeb <154364315+Michael-Adeeb@users.noreply.github.com>
2025-01-17 15:24:58 +0200

    Add files via upload

---
+++ Model/PaymentFactory.php
@@ -0,0 +1,16 @@
+<?php
+require_once "FawryPay.php";
+require_once "VisaPay.php";
+
+class PaymentFactory {
+    public static function createPaymentMethod($type) {
+        switch ($type) {
+            case 'Fawry':
+                return new FawryPay();
+            case 'Visa':
+                return new VisaPay();
+            default:
+                throw new Exception("Invalid payment method type.");
+        }
+    }
+}
